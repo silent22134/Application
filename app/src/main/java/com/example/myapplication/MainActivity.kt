4940b722308212b6cf1b70b578ceadc1eacdfd09
silent22134 <firenba46@gmail.com>
2022-10-28 14:22:55 +0800

    answer

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,17 +21,37 @@ class MainActivity : AppCompatActivity() {
         val tvResult = findViewById<TextView>(R.id.tv_result)
 
         var index = 0
-        val riddleList = mutableListOf("0000")
+        val riddleList = mutableListOf("0000，猜一成語。:四大皆空","0+0=1，猜一成語。:無中生有")
 
 
         edAnswer.text = null
         tvResult.text = null
         btnRiddle.setOnClickListener{
+            edAnswer.text = null
+            tvResult.text = null
             val countRiddle = riddleList.count()
+            index = Random.nextInt(0,countRiddle)
+
             val (riddle,answer) = riddleList[index].split(":")
             tvRiddle.text = "謎題"+riddle
             index++
-            if(index>=countRiddle) index=0
+            if(index>=countRiddle)
+                index=0
+        }
+        btnAnswer.setOnClickListener{
+            if(edAnswer.text.toString() == answer){
+                tvResult.text = "你答對了"
+            }else{
+                tvResult.text = "你答錯了"
+            }
+        }
+        btnReAnswer.setOnClickListener {
+            edAnswer.text = null
+            tvResult.text = null
+        }
+
+        btnShowAnswer.setOnClickListener{
+            tvResult.text = "答案是 :"+answer
         }
     }
 }
